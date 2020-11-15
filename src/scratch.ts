f6637e119817fe21a87fd1308979ed2777e0879c
Richard See <richard.see@argos.co.uk>
2020-11-15 18:18:58 +0000

    Tuple Types and Recursive Type Aliases

--- src/scratch.ts
+++ src/scratch.ts
@@ -13,6 +13,7 @@ const x: {
 const y = x.user.address?.city;
 console.log(x.user.address?.city);
 
+/*------------------------------------------------------------------------------------------------*/
 
 // Nullish Coalescing
 class Foo {
@@ -31,5 +32,54 @@ class Foo {
     console.log(this.#name)
   }
 }
+
+/*------------------------------------------------------------------------------------------------*/
+
+// Tuple types
+// Can now put a spread in the middle
+type Foo2<T extends any[]> = [boolean, ...T, boolean];
+
+/*------------------------------------------------------------------------------------------------*/
+
+// Labelled Tuple Types
+// Array with fixed number of elements
+type Address = [
+  streetNumber: number,
+  city: string,
+  state: string,
+  postal: number
+];
+
+function printAddress(...address: Address) { }
+
+printAddress(122, "San Francisco", "CA", 1231);
+
+/*------------------------------------------------------------------------------------------------*/
+
+// Recursive Typed Alias and Interfaces - can now have a recursive typed value
+
+// Type JSONValue refers to itself
+type JSONValue =
+  | string
+  | number
+  | boolean
+  | null
+  | JSONValue[]
+  | {
+    [k: string]: JSONValue;
+  };
+
+const val: JSONValue = {
+  name: 'r ich',
+  address: {
+    street: 'Spear St'
+  }
+};
+
+/*------------------------------------------------------------------------------------------------*/
+
+// Template type literal
+// Could be good for CSS-in-JS, React onClick functions etc
+type Corner = `${'top' | 'bottom'}-${'left' | 'right'}`;
 
 export * as foo from './data/channels';
