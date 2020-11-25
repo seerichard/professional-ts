cd0d93bcfa0d9d58cc9d34e9de9ded55769d3b2e
Richard See <richard.see@argos.co.uk>
2020-11-25 18:41:56 +0000

    Typing 3rd Party Libraries

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -2,10 +2,9 @@
 
 /**
  * Stringify an Error instance
- * @param {Error} err - The error to stringify
- * @return {string}
+ * @param err - The error to stringify
  */
-function stringifyErrorValue(err) {
+function stringifyErrorValue(err: Error): string {
   return `${err.name.toUpperCase()}: ${err.message}
   ${err.stack || '(no stack trace information)'}`;
 }
@@ -13,17 +12,15 @@ function stringifyErrorValue(err) {
 /**
  * Stringify a thrown value
  *
- * @param {string} errorDescription
- * @param {any} err
- *
- * @return {string}
+ * @param errorDescription
+ * @param err
  */
-export function stringifyError(errorDescription, err) {
-  return `${errorDescription}\n${
-    err instanceof Error
-      ? stringifyErrorValue(err)
-      : err
-      ? '' + err
+export function stringifyError(errorDescription: string, err: any): string {
+  return `${errorDescription}\n${err instanceof Error
+    ? stringifyErrorValue(err)
+    : err
+      // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+      ? '' + err // Stringify the error - coerce into a string
       : '(missing error information)'
-  }`;
+    }`;
 }
